3db582646f768cc275eec6a5785a51c8f4b3bde9
zifengchenll <zifengchen_ll@163.com>
2023-11-13 19:01:16 +0800

    first commit:github_split

---
+++ merge_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rootdir=$(pwd)
+
+workdir=$rootdir/../
+
+while read line || [[ -n ${line} ]]; do
+
+	cd $rootdir
+
+	cd $(dirname ${line}) 
+
+	cat $(basename ${line})"_pkg"* >$(basename ${line}) && rm -rf cat $(basename ${line})"_pkg"* 
+	
+	cd $rootdir && echo $(md5sum ${line})
+
+done <$rootdir/split_log.txt
+
