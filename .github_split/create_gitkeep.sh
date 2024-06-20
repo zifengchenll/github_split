bb6bd3fb024a41619e502c6f2c87a18fe9e8e046
zifengchenll <zifengchen_ll@163.com>
2024-06-20 11:40:02 +0800

    feat: :sparkles: 修正创建空文件过程执行路径问题

--- .github_split/create_gitkeep.sh
+++ .github_split/create_gitkeep.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 获取脚本所在目录
+script_dir=$(dirname "$0")
+
 # 路径文件
-paths_file="paths.txt"
+paths_file="$script_dir/paths.txt"
 
 # 读取路径文件并在每个路径中创建 .gitkeep 文件
 while IFS= read -r path; do
