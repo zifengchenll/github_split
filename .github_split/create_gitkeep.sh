9f481a5af0e6e0b1608f9988abe8e455f48ac7eb
zifengchenll <zifengchen_ll@163.com>
2024-06-20 11:47:06 +0800

    feat: :sparkles: 添加执行权限和修复空行问题

--- .github_split/create_gitkeep.sh
+++ .github_split/create_gitkeep.sh
@@ -8,8 +8,14 @@ paths_file="$script_dir/paths.txt"
 
 # 读取路径文件并在每个路径中创建 .gitkeep 文件
 while IFS= read -r path; do
-  mkdir -p "$path"  # 确保路径存在
-  touch "$path/.gitkeep"  # 创建 .gitkeep 文件
+  # 去掉路径两端的空格
+  path=$(echo "$path" | xargs)
+  
+  # 如果路径不为空则创建目录和 .gitkeep 文件
+  if [ -n "$path" ]; then
+    mkdir -p "$path"  # 确保路径存在
+    touch "$path/.gitkeep"  # 创建 .gitkeep 文件
+  fi
 done < "$paths_file"
 
 echo "所有路径已成功创建 .gitkeep 文件"
